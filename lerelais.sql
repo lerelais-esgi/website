a4a5e0bc68618fb0f3a08a3b788720c910c9c643
Rostan KEZAL <rostan@kezal.fr>
2019-05-14 20:28:31 +0200

    Updated .sql

--- lerelais.sql
+++ lerelais.sql
@@ -16,6 +16,93 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `barecode` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `limit_date` datetime NOT NULL,
+  `user` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`),
+  KEY `user` (`user`),
+  CONSTRAINT `items_ibfk_1` FOREIGN KEY (`type`) REFERENCES `types` (`id`),
+  CONSTRAINT `items_ibfk_2` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `list_items`
+--
+
+DROP TABLE IF EXISTS `list_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `list_items` (
+  `list` int(11) NOT NULL,
+  `item` int(11) NOT NULL,
+  KEY `list` (`list`),
+  KEY `item` (`item`),
+  CONSTRAINT `list_items_ibfk_1` FOREIGN KEY (`list`) REFERENCES `lists` (`id`),
+  CONSTRAINT `list_items_ibfk_2` FOREIGN KEY (`item`) REFERENCES `items` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `list_items`
+--
+
+LOCK TABLES `list_items` WRITE;
+/*!40000 ALTER TABLE `list_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `list_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lists`
+--
+
+DROP TABLE IF EXISTS `lists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_limit` date NOT NULL,
+  `user` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `address` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`),
+  CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lists`
+--
+
+LOCK TABLES `lists` WRITE;
+/*!40000 ALTER TABLE `lists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `newsletter`
 --
 
@@ -42,6 +129,55 @@ LOCK TABLES `newsletter` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock` (
+  `item` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date_limit` date NOT NULL,
+  KEY `item` (`item`),
+  CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`item`) REFERENCES `items` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock`
+--
+
+LOCK TABLES `stock` WRITE;
+/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `types`
+--
+
+DROP TABLE IF EXISTS `types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `types`
+--
+
+LOCK TABLES `types` WRITE;
+/*!40000 ALTER TABLE `types` DISABLE KEYS */;
+INSERT INTO `types` VALUES (1,'TYPES_VEGETABLES'),(2,'TYPES_FRUITS'),(3,'TYPES_WATER'),(4,'TYPES_SYRUP');
+/*!40000 ALTER TABLE `types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -57,20 +193,29 @@ CREATE TABLE `users` (
   `password` blob NOT NULL,
   `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `address` text NOT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `reg_ip` varchar(15) NOT NULL,
-  `last_ip` varchar(15) NOT NULL,
-  `sub` int(11) NOT NULL DEFAULT '0',
-  `level` int(11) NOT NULL DEFAULT  '0',
-  `lang` varchar(2) NOT NULL DEFAULT 'fr',
+  `token` varchar(255) NOT NULL,
+  `reg_ip` varchar(255) NOT NULL,
+  `last_ip` varchar(255) NOT NULL,
+  `sub` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `lang` varchar(2) NOT NULL,
   `validate` int(11) NOT NULL DEFAULT '0',
   `email_token` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,NULL,'Rostan','KEZAL','rostan@kezal.fr','Y‹8Ouå9þ\0ì\0»9oGUQGomzaAkaVanwqmaLQ==','2019-05-09 06:59:48','4 RUe de Verdun\r\n94600 CHoisy-Le-ROi','e37ce7dd6b1345678d33ba87234073b4','','::1',0,0,'',0,'de4b2dad9faa6bc2');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -79,4 +224,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-08 23:51:47
+-- Dump completed on 2019-05-14 20:28:13
